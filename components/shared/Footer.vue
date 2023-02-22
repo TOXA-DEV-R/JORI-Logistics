0624a170d83f5cc5320125ae049b43b92a9b5ddf
TOXIR_DEV <TOXA-DEV-R>
2023-02-22 09:32:37 +0600

    done

---
+++ components/shared/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="footer__wrap">
+        <div class="">
+          <nav class="footer__nav">
+            <div class="footer__brand">
+              <nuxt-img src="/icons/brand/brand-logo.svg" alt="Brand Logo" />
+            </div>
+
+            <div class="footer__menu">
+              <nuxt-link to="" class="footer__link"> </nuxt-link>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
